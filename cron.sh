8dfdb314bfa46b4ac4d65e5efca84533d0b25e2b
Chuck Linart <clinart@gmail.com>
2019-02-13 14:17:01 -0500

    fix cron.sh

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 # add this to your crontab to refresh comics as often as you wish (every 2 hours in prod but you do you)
 
-lynx "http://xkcd.chucklinart.com/write-local-json/blahblahsupertopsecret"
+lynx "[DOMAIN]/write-local-json/blahblahsupertopsecret"
